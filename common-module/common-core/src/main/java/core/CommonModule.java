cc5bc09a80cfd27d0a20a2b36e9cf6ab2af6d8a5
AnastasiaGonzova <gnzv.ololo@gmail.com>
2022-10-10 17:47:04 +0300

    Add PMD plugin
    Add PMD plugin and Java-class for demonstration

---
+++ common-module/common-core/src/main/java/core/CommonModule.java
@@ -0,0 +1,13 @@
+package core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonModule {
+
+    public static void main(String[] args) {
+        int x = 0;
+        SpringApplication.run(CommonModule.class, args);
+    }
+}
